5b9ce1a9d698534704989ac54a50b29da07fef72
laiyingxin <100742127+laiyingxin2@users.noreply.github.com>
2024-02-06 23:51:19 +0800

    Add files via upload

---
+++ Script/GPT-4V_multiimages.py
@@ -0,0 +1,27 @@
+from openai import OpenAI
+
+client = OpenAI()
+response = client.chat.completions.create(
+    model="gpt-4-vision-preview",
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "image_url",
+                    "image_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+                },
+                {
+                    "type": "text",
+                    "text": "What’s in these images? Is there any difference between them?",
+                },
+                {
+                    "type": "image_url",
+                    "image_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+                },
+            ],
+        }
+    ],
+    max_tokens=300,
+)
+print(response.choices[0])
